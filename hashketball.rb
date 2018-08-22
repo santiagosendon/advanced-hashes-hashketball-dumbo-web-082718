fae7686fd0a39e724f4cafc98b3c175712934158
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-22 17:56:24 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -221,5 +221,7 @@ end
 end
 
    max_steals = longest.values.max  # <- .max Returns the object in with the maximum value.
-  
+   max_steals = longest.values.max  # <- .max Returns the object in with the maximum value.
+ longest[longest.key(max_steals)] == max_steals  
+  longest[longest.ley(max_steals)] == max_stea
 end
